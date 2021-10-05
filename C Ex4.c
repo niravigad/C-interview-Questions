421812b540f1c66761f8604acb986ee4f022645e
Nir Avigad <nir290@gmail.com>
2021-10-05 13:24:59 +0300

    Update C Ex4.c

--- C Ex4.c
+++ C Ex4.c
@@ -1,3 +1,10 @@
+/* 
+The following program is supposed to place the "the string" chain into a new piece of memory,
+omitting the character instances - "?"  
+ */
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <malloc.h>
@@ -36,3 +43,22 @@ int main ()
 		printf("\n%s\n sum is: %d",the_string,sum);
 		return 0;
 	}
+
+
+
+/* Answer below 
+
+
+
+
+
+The program does not work properly.
+When we execute the command free (p), the pointer of p will point to an empty cell,
+because we advance it each time inside the loop when we find another character that is not "?" .
+Therefore "free" will cause a runtime error / release memory that is not related to our program.
+The way to solve this is to create a char * temp variable, so that after we do malloc below "if",
+we place the command temp = p, and then we do free (temp),
+Because temp will still point to the first cell.
+
+*/
+
